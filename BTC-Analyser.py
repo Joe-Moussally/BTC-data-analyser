29140b0f5c0864cde4e07e5298f65927265d8982
MJoe111 <joemoussally111@gmail.com>
2022-07-18 21:12:41 +0300

    Calculating Min Max and Avg values for each month

--- BTC-Analyser.py
+++ BTC-Analyser.py
@@ -35,6 +35,31 @@ def analyser():
             value_date = {'year':value[0].year,'month':value[0].month}
             if(date == value_date):
                 values.append(value[1])
-        print('VALUESSS',values)
+
+        avg_value = 0
+        sum = 0
+
+        max_value = 0
+        min_value = values[0]
+
+        for value in values:
+            #compare min value
+            if(value < min_value):
+                min_value = value
+            
+            #compare max value
+            if(value>max_value):
+                max_value = value
+            
+            sum += value
+        
+        avg_value = sum / len(values)
+        print('---------',date['year'],'/',date['month'],'---------')
+        print('Min Value:',min_value)
+        print('Max Value:',max_value)
+        print('Avg Value:',avg_value)
+
+           
+
 
 analyser()
