0f094fa9cb14fa5114c737cdf9e8e26e44196361
MJoe111 <joemoussally111@gmail.com>
2022-07-18 20:16:55 +0300

    Adding function to calculate all time average

--- BTC-Analyser.py
+++ BTC-Analyser.py
@@ -4,6 +4,16 @@ import pandas as pd
 def analyser():
     print('TEST')
     data = pd.read_excel(io='./csv example.xlsx',sheet_name='BAVERAGE-USD-Sols')
-    print(data)
+
+    #transforming dataframe to list
+    data_list = data.values.tolist()
+    print(data_list[0][1])
+
+    #calculating the all time average
+    sum = 0
+    for element in data_list:
+        sum += element[1]
+    all_time_average = sum / (len(data_list))
+    print(all_time_average)
 
 analyser()
